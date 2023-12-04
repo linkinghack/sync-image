9528bc2b15cd06f6db24177737dd3ed7d2e6ee45
LiuLei <liuleiit01@chinamobile.com>
2023-12-04 12:05:10 +0800

    support windows powershell

--- sync.py
+++ sync.py
@@ -1,26 +1,53 @@
 import argparse
 import json
 import subprocess
+import platform
 from typing import List
 
-#  command template
-tmpl = """
+
+powershell_preconfig = """
+$ErrorActionPreference = "Stop"
+$ErrorView = "NormalView"
+Set-StrictMode -Version Latest
+
+"""
+
+bash_preconfig = """
+#!/bin/bash
 set -xeu
-docker pull --platform=linux/arm64 ${image_repo}/${image_name}
-docker tag  ${image_repo}/${image_name} ${private_repo}/${image_name}-arm64
-docker push ${private_repo}/${image_name}-arm64
 
-docker pull --platform=linux/amd64 ${image_repo}/${image_name}
-docker tag  ${image_repo}/${image_name} ${private_repo}/${image_name}-amd64
-docker push ${private_repo}/${image_name}-amd64
+"""
 
-docker manifest rm ${private_repo}/${image_name} || echo "manifest does not exists, now create it"
-docker manifest create ${private_repo}/${image_name} ${private_repo}/${image_name}-arm64 ${private_repo}/${image_name}-amd64
-docker manifest push ${private_repo}/${image_name}
+#  command template
+## for push an image immediately after pull and retag.
+tmpl_immediate_sync = """
+${shell_preconfig}
+${container_cli} pull --platform=linux/arm64 ${image_repo}/${image_name}
+${container_cli} tag  ${image_repo}/${image_name} ${private_repo}/${image_name}-arm64
+${container_cli} push ${private_repo}/${image_name}-arm64
+
+${container_cli} pull --platform=linux/amd64 ${image_repo}/${image_name}
+${container_cli} tag  ${image_repo}/${image_name} ${private_repo}/${image_name}-amd64
+${container_cli} push ${private_repo}/${image_name}-amd64
+
+${container_cli} manifest rm ${private_repo}/${image_name} || echo "manifest does not exists, now create it"
+${container_cli} manifest create ${private_repo}/${image_name} ${private_repo}/${image_name}-arm64 ${private_repo}/${image_name}-amd64
+${container_cli} manifest push ${private_repo}/${image_name}
+"""
+
+## for temporarily cache image in local repo
+tmpl_cache_locally = """
+
 """
 
 
-def sync_image(img_list: List[str], target_repo: str):
+def sync_image(img_list: List[str], target_repo: str, container_cli: str):
+    # check current OS type and set preconfig script
+    shell_preconfig = bash_preconfig
+    if (platform.system() == "Windows"):
+        shell_preconfig = powershell_preconfig
+    script = tmpl_immediate_sync.replace("${shell_preconfig}", shell_preconfig)
+
     for img in img_list:
         #  split repo and name
         repo_end_idx = img.find('/')
@@ -30,18 +57,34 @@ def sync_image(img_list: List[str], target_repo: str):
         print('image_name=', image_name, 'image_repo=', image_repo)
 
         # sync multi-arch
-        script = tmpl.replace("${image_repo}", image_repo)
+        script = script.replace("${image_repo}", image_repo)
         script = script.replace("${image_name}", image_name)
         script = script.replace("${private_repo}", target_repo)
-        tmpSh = open('tmp.sh', 'w')
-        tmpSh.write(script)
-        tmpSh.close()
+        script = script.replace("${container_cli}", container_cli)
+        # tmpSh = open('tmp.sh', 'w')
+        # tmpSh.write(script)
+        # tmpSh.close()
         # exec shell
-        subprocess.call(["bash", "tmp.sh"])
+        match platform.system():
+            case "Windows": 
+                tmpSh = open('tmp.ps1', 'w')
+                tmpSh.write(script)
+                tmpSh.close()
+                subprocess.call(["powershell", "./tmp.ps1"])
+            case _ :
+                tmpSh = open('tmp.sh', 'w')
+                tmpSh.write(script)
+                tmpSh.close()
+                subprocess.call(["bash", "tmp.sh"])
+
+# def local_cache_images(img_list: List[str]):
+#     for img in img_list:
+        
 
 arg_parser = argparse.ArgumentParser('sync-image')
-arg_parser.add_argument('-r', '--repo')
-arg_parser.add_argument('-l', '--imagelist', default='list.json')
+arg_parser.add_argument('-r', '--target-repo', required=True, type=str, dest='repo')
+arg_parser.add_argument('-l', '--image-list', default='list.json', dest='imagelist', type=str)
+arg_parser.add_argument('-c' '--container-cli', default='docker', dest='container_cli', choices=['docker', 'podman'], type=str) # podman or docker
 
 def main():
     target_repo = "docker.io"
@@ -56,8 +99,8 @@ def main():
     print("private_repo: " + target_repo)
     lf = open(list_file, 'r')
     image_list = json.load(lf)
-    print(image_list)
-    sync_image(image_list, target_repo)
+    print(f"image list: {image_list}")
+    sync_image(image_list, target_repo, parsed.container_cli)
 
 if __name__ == "__main__":
     main()
